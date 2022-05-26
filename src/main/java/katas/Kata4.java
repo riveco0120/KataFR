2ad037455cfe9442ce2c10140cbf06bac75a3004
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-25 21:31:36 -0500

    kata4
    Se implementaron los metodos flatMap y map para la kata 4

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -2,12 +2,14 @@ package katas;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultimap;
 import model.BoxArt;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -17,7 +19,9 @@ import java.util.Map;
 public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
-
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        List<Map> nuevaList =movieLists.stream().flatMap(item->item.getVideos().stream()).
+               map(item2 -> ImmutableMap.of("id", item2.getId(), "title", item2.getTitle(), "boxart", new BoxArt(150, 200, item2.getUri())))
+               .collect(Collectors.toList());
+        return nuevaList;
     }
 }
